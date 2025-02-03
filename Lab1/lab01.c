b687f0ab56275af6ae1e5568c5e7f588bbcf1791
franbinoaldrino <faldridge071@gmail.com>
2025-02-02 21:14:17 -0500

    Lab 1 files

---
+++ Lab1/lab01.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int function1(int a, int b) {
+	
+	// returns a + b + 3
+	int i = 0;
+	int c = a + b;
+
+	while (i < 3) {
+		c = c + i;
+		++i;
+	}
+
+	printf("function1 returns: %d\n", c);
+	return c;
+}
+
+int main(int argc, char** argv) {
+	int result;
+	int i = 0;
+	
+	result = function1(3,5); // returns 11
+
+	while (i < 2) {
+		result = result * 2;
+		++i;
+	}	// returns 44
+
+	printf("main returns: %d\n", result); 
+}
---
+++ Lab1/lab01.s
@@ -0,0 +1,107 @@
+	.file	"lab01.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"function1 returns: %d\n"
+	.text
+	.globl	function1
+	.type	function1, @function
+function1:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	$0, -8(%rbp)
+	movl	-20(%rbp), %edx
+	movl	-24(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, -4(%rbp)
+	jmp	.L2
+.L3:
+	movl	-8(%rbp), %eax
+	addl	%eax, -4(%rbp)
+	addl	$1, -8(%rbp)
+.L2:
+	cmpl	$2, -8(%rbp)
+	jle	.L3
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	-4(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	function1, .-function1
+	.section	.rodata
+.LC1:
+	.string	"main returns: %d\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movq	%rsi, -32(%rbp)
+	movl	$0, -4(%rbp)
+	movl	$5, %esi
+	movl	$3, %edi
+	call	function1
+	movl	%eax, -8(%rbp)
+	jmp	.L6
+.L7:
+	sall	-8(%rbp)
+	addl	$1, -4(%rbp)
+.L6:
+	cmpl	$1, -4(%rbp)
+	jle	.L7
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC1(%rip), %rax
+	movq	%rax, %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 13.2.0-23ubuntu4) 13.2.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
